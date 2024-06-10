6b1a8911ded4902d6946bd3bd8de38cedeffa587
Ken Duenwald <kenduenwald@liferay.com>
2024-06-10 13:16:43 -0700

    LPD-28065 New test for creating announcement entry

--- modules/test/playwright/pages/announcements-web/AnnouncementsPage.ts
+++ modules/test/playwright/pages/announcements-web/AnnouncementsPage.ts
@@ -6,18 +6,27 @@
 import {Locator, Page} from '@playwright/test';
 
 import {ApplicationsMenuPage} from '../product-navigation-applications-menu/ApplicationsMenuPage';
+import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 
 export class AnnouncementsPage {
 	readonly page: Page;
 
 	readonly applicationsMenuPage: ApplicationsMenuPage;
+	readonly contentFrameLocator: FrameLocator;
+	readonly contentTextBox: Locator;
 	readonly newButton: Locator;
+	readonly saveButton: Locator;
+	readonly titleField: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
 
 		this.applicationsMenuPage = new ApplicationsMenuPage(page);
+		this.contentFrameLocator = page.frameLocator('iframe');
+		this.contentTextBox = this.contentFrameLocator.getByRole('textbox');
 		this.newButton = page.getByRole('link', {name: 'Add Announcement'});
+		this.saveButton = page.getByRole('button', {name: 'Save'});
+		this.titleField = page.getByText('Title');
 	}
 
 	async goto() {
@@ -27,5 +36,26 @@ export class AnnouncementsPage {
 	async goToCreateNewAnnouncement() {
 		await this.goto();
 		await this.newButton.click();
+	}
+
+	async saveNewAnnouncement(content: string, title: string) {
+		await this.titleField.fill(title);
+		await this.contentTextBox.fill(content);
+		await this.saveButton.click();
+	}
+
+	async deleteAnnouncement(name: string) {
+		await this.goto();
+
+		await this.page.getByRole('row', {name}).getByTitle('Actions').click();
+		this.page.once('dialog', (dialog) => {
+			dialog.accept();
+		});
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: this.page.getByRole('link', {name: 'Delete'}),
+			trigger: this.page.getByRole('row', {name}).getByTitle('Actions'),
+		});
 	}
 }
